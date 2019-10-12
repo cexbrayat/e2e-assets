b4dd3b195e91cf949477b3ee9ed8d3ff2fe4e292
cexbrayat <cedric@ninja-squad.com>
2019-10-12 09:49:48 +0200

    add image

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('e2e-assets app is running!');
+    expect(page.getImage()).not.toBeNull();
   });
 
   afterEach(async () => {
